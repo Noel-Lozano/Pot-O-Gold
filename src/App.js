fc5c472f27b3c12fbd6e2d538c6af4871f5ab99a
Noel Lozano <noelmlozano6@gmail.com>
2025-10-25 21:37:00 -0600

    Delete src directory

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import FirebaseAuthApp from './firebase-auth/FirebaseAuthApp';
-import Pot_O_GoldDashboard from './components/Pot_O_GoldDashboard';
-
-function App() {
-  return <FirebaseAuthApp />;
-}
-
-export default App;
